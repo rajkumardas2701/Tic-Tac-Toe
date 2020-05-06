efdf88380ebe8fbe4add979489281f2f857db317
evansinho <igiri.evanson@gmail.com>
2020-05-06 16:01:14 +0100

    added login to the main.rb file

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,37 +1,49 @@
 class Board
-  def initialize(board, win_criteria)
-    @board = board
-    @win_criteria = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-  [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+  attr_reader :board
+
+  def initialize
+    @board = %w[
+      _ _ _
+      _ _ _
+      _ _ _
+    ]
+
+    @win_criteria = [
+      [0, 1, 2], 
+      [3, 4, 5], 
+      [6, 7, 8], 
+      [0, 3, 6],
+      [1, 4, 7], 
+      [2, 5, 8], 
+      [0, 4, 8], 
+      [2, 4, 6]
+    ]
   end
-  
-  board = w%[- - - - - - - - -]
 
   def update_board(position, value)
-    board[position.to_i - 1] = value
+    @board[position.to_i - 1] = value
   end
   
-  def game_won?(choice, won = false)
-    win_criteria.each do |x|      
-      x.each do |y|
-        i = 0
-        while i<= 2 
-        if board[y] != choice
-          return false
-        else
-          next
-        end
-        return true
-      end
+  # def player_won?(choice, value=false)
+  #   @win_criteria.each do |x|      
+  #     x.each do |y|
+  #       i = 0
+  #       while i<= 2 
+  #         if @board[y] != choice
+  #           return false
+  #         else
+  #           next
+  #         end
+  #       end
+  #       return true 
+  #     end
+  #   end
+  # end
 
+  def full?
+    @board.each do |x|
+      return false if x == '_'
+      true
     end
   end
-
-  def continue_game?
-    board.each do |x|
-      return if x == '-'? true : false
-    end
-
-  end
-
 end
--- lib/Validation.rb
+++ lib/Validation.rb
@@ -1,15 +1,15 @@
 class Validation < Board
-  def initialize(valid_option, selected_positions)
-    valid_options = %w[1 2 3 4 5 6 7 8 9]
-    selected_positions = []
+  def initialize
+    @valid_options = %w[1 2 3 4 5 6 7 8 9]
+    @selected_positions = []
   end
 
   def valid_input(position)
-    return valid_options.include?(option_selected) && !selected_positions.include?(option_selected)
+    return @valid_options.include?(position) && !@selected_positions.include?(position)
   end
 
   def update_selected_position(position)
-    selected_positions.push(option_selected)
+    @selected_positions.push(position)
   end
 
 end
