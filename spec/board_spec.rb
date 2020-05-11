c0bd92b3a5292ebb2ff74e2495f6ca6b37ca119f
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-05-11 18:32:09 +0530

    added test for board full and condition for draw in player_won?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,18 +18,51 @@ describe Board do
       expect(tictactoe.player_won?).to eq(true)
     end
 
-    it 'should return true if a player match 3 rows' do
+    it 'should return true if a player match 3 columns' do
       tictactoe.update_board(1,'X')
       tictactoe.update_board(4,'X')
       tictactoe.update_board(7,'X')
       expect(tictactoe.player_won?).to eq(true)
     end
 
-    it 'should return true if a player match 3 rows' do
+    it 'should return true if a player match 2 diagonals' do
       tictactoe.update_board(1,'X')
       tictactoe.update_board(5,'X')
       tictactoe.update_board(9,'X')
       expect(tictactoe.player_won?).to eq(true)
+    end
+
+    it 'should return false if the game ends in draw' do
+      tictactoe.update_board(1,'X')
+      tictactoe.update_board(2,'O')
+      tictactoe.update_board(3,'X')
+      tictactoe.update_board(4,'O')
+      tictactoe.update_board(5,'O')
+      tictactoe.update_board(6,'X')
+      tictactoe.update_board(7,'O')
+      tictactoe.update_board(8,'X')
+      tictactoe.update_board(9,'O')
+      expect(tictactoe.player_won?).to eq(false)
+    end
+
+  end
+
+  describe 'full?' do  
+    it 'should return true if the board is full' do
+      tictactoe.update_board(1,'X')
+      tictactoe.update_board(2,'O')
+      tictactoe.update_board(3,'X')
+      tictactoe.update_board(4,'O')
+      tictactoe.update_board(5,'O')
+      tictactoe.update_board(6,'X')
+      tictactoe.update_board(7,'O')
+      tictactoe.update_board(8,'X')
+      tictactoe.update_board(9,'O')
+      expect(tictactoe.full?).to eq(true)
+    end
+    
+    it 'should return false if the board is not full' do
+      expect(tictactoe.full?).to eq(false)
     end
   end
 end
